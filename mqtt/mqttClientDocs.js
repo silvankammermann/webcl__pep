3dd48d1d653ab6aa5b24d4f81c78fb4aa4e596a8
Silvan Kammermann <silvan.kammermann@students.fhnw.ch>
2024-01-06 15:39:27 +0100

    complete docs

--- mqtt/mqttClientDocs.js
+++ mqtt/mqttClientDocs.js
@@ -1,6 +1,28 @@
 /**
+ * @callback onMessageArrivedCallback
+ * @param    {!Message} message - the message that arrived
+ * @return   { void }
+ */
+
+/**
+ *
+ * @typedef subscribe
+ * @param {String} topic - the topic to subscribe to
+ * @param {onMessageArrivedCallback} callback - the callback to register
+ * @return { void }
+ */
+
+/**
+ * @function publish
+ * @param {String} topic   - the topic to publish to
+ * @param {String} message - the message to publish
+ * @return { void }
+ */
+
+/**
  * Data structure that exposes MQTT functions to the client.
+ *
  * @typedef MqttClient
- * @property {Function} subscribe - register a callback for a topic
- * @property {Function} publish   - publish a message to a topic
+ * @property { subscribe } subscribe - register a callback for a topic
+ * @property { publish } publish     - publish a message to a topic
  */
